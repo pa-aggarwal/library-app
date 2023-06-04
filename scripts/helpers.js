79c2f8fbff670f1be1eab5cf97441b64fcf3ef7e
Priya Aggarwal <priya.aggarwal0125@gmail.com>
2023-06-03 23:45:46 -0400

    Add functions to make and display books in a table.
    Create separate helpers.js file to handle creating HTML nodes in the
    table and manipulating the DOM. Change type of main script to module
    for importing functions from helpers.js file.

---
+++ scripts/helpers.js
@@ -0,0 +1,56 @@
+const helpers = function helpers() {
+
+    function checkInnerLengths(array, length) {
+        return array.every(innerArray => innerArray.length === length);
+    }
+
+    function makeTableHeader(columns) {
+        const header = document.createElement("thead");
+        const row = document.createElement("tr");
+        columns.forEach(column => {
+            const columnHTML = `<th scope="col">${column}</th>`;
+            row.insertAdjacentHTML("beforeend", columnHTML);
+        });
+        header.appendChild(row);
+        return header;
+    }
+
+    function makeTableBody(dataRows) {
+        const tbody = document.createElement("tbody");
+        const fragment = new DocumentFragment();
+        dataRows.forEach((row) => {
+            const trow = document.createElement("tr");
+            row.forEach(cell => {
+                trow.insertAdjacentHTML("beforeend", `<td>${cell}</td>`);
+            });
+            fragment.appendChild(trow);
+        });
+        tbody.appendChild(fragment);
+        return tbody;
+    }
+
+    /**
+     * Create a new table object.
+     * @param {Array<string>} headerColumns
+     * @param {Array<Array>} rows
+     */
+    function Table(headerColumns, rows) {
+        if (!checkInnerLengths(rows, headerColumns.length)) {
+            throw new Error("Length of each row must equal length of headers.");
+        }
+        this.headerColumns = headerColumns;
+        this.rows = rows;
+    }
+
+    Table.prototype.getElement = function display() {
+        const myContainer = document.createElement("table");
+        const header = makeTableHeader(this.headerColumns);
+        const body = makeTableBody(this.rows);
+        myContainer.append(header, body);
+        return myContainer;
+    };
+
+    return {Table};
+}();
+
+export default helpers;
